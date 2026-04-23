2c8aaa5d8fa99fbf078eaa1b5a1c04f8e390e65a
Mostafa Anwari <manwari@owu.edu>
2026-04-22 23:36:43 -0400

    feat: add home stats trio section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { HomeHero } from "./_components/HomeHero";
 import { HomeMissionBand } from "./_components/HomeMissionBand";
+import { HomeStatsTrio } from "./_components/HomeStatsTrio";
 
 export default function Home() {
   return (
     <>
       <HomeHero />
       <HomeMissionBand />
+      <HomeStatsTrio />
     </>
   );
 }
